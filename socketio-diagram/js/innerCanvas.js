8ef3afc93c7dbd3b9326bba7ee9654210ba80233
duke from earth <stephen_duke@live.com>
2020-07-04 16:15:01 -0400

    Add canvas message

--- socketio-diagram/js/innerCanvas.js
+++ socketio-diagram/js/innerCanvas.js
@@ -10,6 +10,7 @@ export default class smallCanvas {
         this.y = y;
         this.width = 150;
         this.height = 150;
+        this.msg = "CANVAS";
         this.code = code;
     }
     moveRect(data) {
@@ -20,6 +21,15 @@ export default class smallCanvas {
         ctx.fillStyle = 'red';
         ctx.fillRect(this.block.x - 75, this.block.y, this.block.width, this.block.height);
         ctx.fillStyle = 'black';
+        ctx.save();
+        ctx.font = "20px Verdana";
+        let txtWidth = ctx.measureText(this.msg).width;
+
+        ctx.fillStyle = 'black';
+        ctx.fillRect(this.x - 100, this.y + 170, 200, 45);
+        ctx.fillStyle = 'white';
+        ctx.fillText(this.msg, this.x - txtWidth / 2, this.y + 200);
+        ctx.restore();
     }
     collides(object) {
         if (object.x < this.x + this.width / 2 &&
