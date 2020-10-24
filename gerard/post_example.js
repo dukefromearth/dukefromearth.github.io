f0ad9b9125962b28f3de9d7f82e66622879f4142
duke from earth <stephen_duke@live.com>
2020-10-24 17:48:48 -0400

    Update post_example.js

--- gerard/post_example.js
+++ gerard/post_example.js
@@ -4,7 +4,7 @@ async function postData(url = '', data = {}) {
     const response = await fetch(url, {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
-        // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         headers: {
             'Content-Type': 'application/json'
             // 'Content-Type': 'application/x-www-form-urlencoded',
